1329f1d37d5e9b3cc139dba4553f18f7f113473a
tgg <qazplmtfgcv123@gmail.com>
2025-03-18 11:05:55 +0900

    chore : health check api 작성

---
+++ src/main/java/org/example/expert/config/HealthCheckController.java
@@ -0,0 +1,17 @@
+package org.example.expert.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck(){
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+
+}
